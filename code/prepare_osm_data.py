0706b2f72f8b13bf38a73fe65a6269beba4c2c1a
Abhijit <abhijit@LenovoLaptop>
2024-07-16 17:07:03 +0200

    Added Germany city population

--- code/prepare_osm_data.py
+++ code/prepare_osm_data.py
@@ -2,9 +2,30 @@ import pandas as pd
 
 def main():
     #get the city population data
-    df_city_p = pd.read_csv('../data/city_population',delimiter=';')
-    print(df_city_p.head())
+
+    df_city_p = pd.read_csv('./data/city_population.csv',delimiter=';'
+                            ,encoding='latin1',header=None)
+    
+    #drop other columns, only take the 2023 data
+    df_city_p = df_city_p.drop(columns=[0,2,3,4,5])
+
+    #change column name
+    col_name = {1:'city', 6:'population'}
+
+    df_city_p.rename(columns=col_name,inplace=True)
+
+    # change data type of population to int
+    df_city_p['population'] = df_city_p['population'].astype('Int64')
+
+    #Consider only the cities with population more than 100000
+    df_city_p = df_city_p[df_city_p['population']>=100000]
 
 
-if __name__ == 'main':
+    print(df_city_p.info())
+    # print("Hello")
+
+def get_city_from_df(str):
+    str = str.split(',')
+
+if __name__ == '__main__':
     main()
